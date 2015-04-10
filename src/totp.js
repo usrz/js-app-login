eb6e959a2b31b2c987132fab0b208ef47d125448
Pier Fumagalli <pier@usrz.com>
2015-04-11 00:56:56 +0900

    TOTP implementation.

---
+++ src/totp.js
@@ -0,0 +1,122 @@
+'use strict';
+
+var base32 = require('./base32');
+var crypto = require('crypto');
+var util = require('util');
+
+/* =========================================================================== */
+
+function Token(options) {
+  if (!(this instanceof Token)) return new Token(options);
+
+  // Constructed with a label
+  if (util.isString(options)) {
+    options = { label: options };
+  } else if (! util.isObject(options)) {
+    throw new TypeError('Must be constructed with a label string or options object');
+  }
+
+  var label = options.label || null;
+  var issuer = options.issuer || null;
+  var secret = options.secret || null;
+  var algorithm = options.algorithm || 'sha1';
+  var digits = options.digits || 6;
+  var period = options.period || 30;
+
+  // Secret must be a string or buffer
+  if (! secret) secret = crypto.randomBytes(32);
+  if (util.isString(secret)) secret = base32.decode(secret);
+  if ((!util.isBuffer(secret)) || (secret.length < 1)) {
+    throw new TypeError('The \'secret\' must be a non-empty Buffer or a base32 string');
+  }
+
+  // Label must be specified
+  if (! util.isString(label)) {
+    throw new TypeError('The \'label\' must be a string');
+  }
+
+  // Issuer must be a string or unspecified
+  if (issuer && (! util.isString(issuer))) {
+    throw new TypeError('The \'issuer\' must be a string');
+  }
+
+  // Algorithm normalization
+  if (util.isFunction(algorithm.toLowerCase)) algorithm = algorithm.toLowerCase();
+  if (['sha1', 'sha256', 'sha512'].indexOf(algorithm) < 0) {
+    throw new TypeError('Unsupported algorithm \'' + algorithm + '\'');
+  }
+
+  // Check digits
+  if ((! util.isNumber(digits)) || (digits < 6) || (digits > 8)) {
+    throw new TypeError('The \'digits\' must be between 6 and 8');
+  }
+
+  // Period validation
+  if (! util.isNumber(period)) {
+    throw new TypeError('The \'period\' must be a number');
+  } else if ((period < 15) || (period > 120)) {
+    throw new TypeError('The \'period\' must be from 15 to 120 seconds');
+  }
+
+  // Read-only properties
+  Object.defineProperties(this, {
+    'type':      { enumerable: true, configurable: false, value: 'totp' },
+    'algorithm': { enumerable: true, configurable: false, value: algorithm },
+    'label':     { enumerable: true, configurable: false, value: label },
+    'digits':    { enumerable: true, configurable: false, value: digits },
+    'period':    { enumerable: true, configurable: false, value: period },
+    'secret':    { enumerable: true, configurable: false, get: function() {
+      return new Buffer(secret); // always clone
+    }}
+  });
+
+  // Optional issuer
+  if (issuer) Object.defineProperty(this, 'issuer', {
+    enumerable: true, configurable: false, value: issuer
+  });
+}
+
+Token.prototype.compute = function compute(at) {
+  if (! at) at = new Date().getTime();
+  else if (util.isDate(at)) at = at.getTime();
+  else if (! util.isNumber(at)) throw new Error('Must be called with a date (or ms from epoch)');
+
+  // Our counter as a 8-byte big-endian number
+  var timeslot = Math.floor(at / 1000 / this.period);
+  var counter = new Buffer(8).fill(0);
+  counter.writeUInt32BE(timeslot, 4);
+
+  // Our HMAC derived from the counter
+  var hmac = crypto.createHmac(this.algorithm, this.secret)
+                   .update(counter)
+                   .digest();
+
+  // Calculate the offset of the 4-bytes to read
+  var offset = hmac[hmac.length - 1] & 0x0f;
+  var sbits = hmac.readUInt32BE(offset) & 0x07fffffff;
+
+  // Calculate and pad the number displayed on the TOTP generator
+  var number = (sbits % (Math.pow(10, this.digits))).toString();
+  while (number.length < this.digits) number = '0' + number;
+
+  // Done!
+  return number;
+}
+
+Token.prototype.toString = function toString() {
+  var string = "otpauth://totp/";
+
+  if (this.issuer != null) string += encodeURI(this.issuer) + ':';
+
+  string += encodeURI(this.label);
+  string += '?secret=' + base32.encode(this.secret);
+
+  if (this.issuer != null)      string += '&issuer='    + encodeURIComponent(this.issuer);
+  if (this.algorithm != 'sha1') string += '&algorithm=' + this.algorithm.toUpperCase();
+  if (this.period != 30)        string += '&period='    + this.period;
+  if (this.digits != 6)         string += '&digits='    + this.digits;
+
+  return string;
+}
+
+exports = module.exports = Token;
---
+++ test/totp.test.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Token = require('../src/totp');
+
+var testVectorsHOTP = [ '755224', '287082', '359152', '969429', '338314',
+                        '254676', '287922', '162583', '399871', '520489' ];
+
+var testVectorsTOTP = [
+  [ new Date('1970-01-01T00:00:59.000Z'), 'SHA1',   '94287082', '12345678901234567890'                                             ],
+  [ new Date('1970-01-01T00:00:59.000Z'), 'SHA256', '46119246', '12345678901234567890123456789012'                                 ],
+  [ new Date('1970-01-01T00:00:59.000Z'), 'SHA512', '90693936', '1234567890123456789012345678901234567890123456789012345678901234' ],
+  [ new Date('2005-03-18T01:58:29.000Z'), 'SHA1',   '07081804', '12345678901234567890'                                             ],
+  [ new Date('2005-03-18T01:58:29.000Z'), 'SHA256', '68084774', '12345678901234567890123456789012'                                 ],
+  [ new Date('2005-03-18T01:58:29.000Z'), 'SHA512', '25091201', '1234567890123456789012345678901234567890123456789012345678901234' ],
+  [ new Date('2005-03-18T01:58:31.000Z'), 'SHA1',   '14050471', '12345678901234567890'                                             ],
+  [ new Date('2005-03-18T01:58:31.000Z'), 'SHA256', '67062674', '12345678901234567890123456789012'                                 ],
+  [ new Date('2005-03-18T01:58:31.000Z'), 'SHA512', '99943326', '1234567890123456789012345678901234567890123456789012345678901234' ],
+  [ new Date('2009-02-13T23:31:30.000Z'), 'SHA1',   '89005924', '12345678901234567890'                                             ],
+  [ new Date('2009-02-13T23:31:30.000Z'), 'SHA256', '91819424', '12345678901234567890123456789012'                                 ],
+  [ new Date('2009-02-13T23:31:30.000Z'), 'SHA512', '93441116', '1234567890123456789012345678901234567890123456789012345678901234' ],
+  [ new Date('2033-05-18T03:33:20.000Z'), 'SHA1',   '69279037', '12345678901234567890'                                             ],
+  [ new Date('2033-05-18T03:33:20.000Z'), 'SHA256', '90698825', '12345678901234567890123456789012'                                 ],
+  [ new Date('2033-05-18T03:33:20.000Z'), 'SHA512', '38618901', '1234567890123456789012345678901234567890123456789012345678901234' ],
+  [ new Date('2603-10-11T11:33:20.000Z'), 'SHA1',   '65353130', '12345678901234567890'                                             ],
+  [ new Date('2603-10-11T11:33:20.000Z'), 'SHA256', '77737706', '12345678901234567890123456789012'                                 ],
+  [ new Date('2603-10-11T11:33:20.000Z'), 'SHA512', '47863826', '1234567890123456789012345678901234567890123456789012345678901234' ],
+];
+
+describe('TOTP', function() {
+
+  describe('RFC-4226 test vectors', function() {
+
+    // HOTP / RFC-4266 test vectors with precalculated time -> counter
+    for (var i = 0; i < testVectorsHOTP.length; i++) (function(i) {
+      var timestamp = (i * 1000 * 30) + 1; // add 1, math precision on floats
+      var expected = testVectorsHOTP[i];
+
+      var details = '(HOTP SHA1 with counter ' + i + ')';
+      it('should validate test vector ' + i + ' ' + details, function() {
+        var token = new Token({
+          label: 'foo',
+          secret: new Buffer('12345678901234567890', 'utf8'),
+          period: 30,
+          digits: 6
+        });
+
+        expect(token.compute(timestamp)).to.equal(expected);
+      });
+    })(i);
+  });
+
+  describe('RFC-6238 test vectors', function() {
+
+    // TOTP / RFC-6238 test vectors
+    for (var i = 0; i < testVectorsTOTP.length; i++) (function(i) {
+      var test = testVectorsTOTP[i];
+      var timestamp = test[0];
+      var algorithm = test[1];
+      var expected = test[2];
+      var secret =  new Buffer(test[3], 'utf8');
+
+      var details = '(TOTP+' + algorithm + ' at ' + timestamp.toISOString() + ')';
+      it('should validate test vector ' + i + ' ' + details, function() {
+        var token = new Token({
+          label: 'foo',
+          algorithm: algorithm,
+          secret: secret,
+          period: 30,
+          digits: 8
+        });
+
+        expect(token.compute(timestamp)).to.equal(expected);
+      });
+    })(i);
+  });
+
+  // Just check with Google Authenticator... RFC-6238 makes assumptions when dealing with
+  // SHA256 and 512 (the secret is *NOT* 12345678901234567890, but a repetition thereof
+  describe('Google Validator compliance (results generated manually - see comments)', function() {
+
+    it('should validate a known value with a long secret', function() {
+      var secret = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()';
+      var buffer = new Buffer(secret, 'utf8');
+      var token = new Token({
+        label: 'Long secret',
+        issuer: 'USRZ Tests',
+        algorithm: 'SHA512',
+        secret: buffer,
+        period: 120,
+        digits: 8
+      });
+
+      // A QR code for updating tests...
+      // console.log('Scan the code at https://chart.googleapis.com/chart?chs=500x500&cht=qr&choe=UTF-8&chl=' + encodeURIComponent(token.toString()));
+      // console.log('Now the time is ' + new Date().toISOString());
+      // console.log('Now the token is ' + token.compute());
+
+      // Timestamp when test was made, and associated result
+      var timestamp = new Date('2015-04-10T15:49:18.000Z');
+      var result = '2693' + '9240';
+
+      // Check
+      expect(token.compute(timestamp)).to.equal(result);
+    });
+
+    it('should validate a known value with a long secret', function() {
+      var secret = 'a';
+      var buffer = new Buffer(secret, 'utf8');
+      var token = new Token({
+        label: 'Short secret',
+        issuer: 'USRZ Tests',
+        algorithm: 'SHA512',
+        secret: buffer,
+        period: 15,
+        digits: 7
+      });
+
+      // A QR code for updating tests...
+      // console.log('Scan the code at https://chart.googleapis.com/chart?chs=500x500&cht=qr&choe=UTF-8&chl=' + encodeURIComponent(token.toString()));
+      // console.log('Now the time is ' + new Date().toISOString());
+      // console.log('Now the token is ' + token.compute());
+
+      // Timestamp when test was made, and associated result
+      var timestamp = new Date('2015-04-10T15:49:18.001Z');
+      var result = '497' + '3263';
+
+      // Check
+      expect(token.compute(timestamp)).to.equal(result);
+    });
+
+
+  });
+
+});
