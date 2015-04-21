27c875474b75daadf83d00ae3dcbcf65782f8d49
Pier Fumagalli <pier@usrz.com>
2015-04-22 01:04:07 +0900

    EC Keys parsing/serializing and tests.

--- ecdh.js
+++ ecdh.js
@@ -44,7 +44,7 @@ var curves = {
  * ASN.1                                                                      *
  * ========================================================================== */
 
-var ASN1ECOpenSSLKey = asn.define('OpenSSLKey', function() {
+var ASN1ECRfc5915Key = asn.define('Rfc5915Key', function() {
   this.seq().obj(
     this.key('version').int(),
     this.key('privateKey').octstr(),
@@ -61,7 +61,7 @@ var ASN1ECPkcs8Key = asn.define('Pkcs8Key', function() {
   this.seq().obj(
     this.key('version').int(),
     this.key('algorithmIdentifier').seq().obj(
-      this.key('publicKeyType').objid({
+      this.key('privateKeyType').objid({
         '1 2 840 10045 2 1': 'EC'
       }),
       this.key('parameters').objid({
@@ -113,7 +113,7 @@ function parsePublicKeyBuffer(curve, buffer) {
 /* Parse PKCS8 from RFC 5208 */
 function parsePkcs8(buffer) {
   var key = ASN1ECPkcs8Key.decode(buffer, 'der');
-  var privateKeyWrapper = ASN1ECOpenSSLKey.decode(key.privateKey, 'der');
+  var privateKeyWrapper = ASN1ECRfc5915Key.decode(key.privateKey, 'der');
   var curve = key.algorithmIdentifier.parameters;
   var bytes = lengths[curve];
 
@@ -131,8 +131,9 @@ function parsePkcs8(buffer) {
   return components;
 }
 
-function parseOpenSSL(buffer) {
-  var key = ASN1ECOpenSSLKey.decode(buffer, 'der');
+/* Parse EC from RFC 5915 */
+function parseRfc5915(buffer) {
+  var key = ASN1ECRfc5915Key.decode(buffer, 'der');
   var bytes = lengths[key.parameters];
 
   var privateKey = key.privateKey;
@@ -159,7 +160,7 @@ function parseSpki(buffer) {
  * PEM PARSING                                                                *
  * ========================================================================== */
 
-var pemOpenSSLRE = /-+BEGIN EC PRIVATE KEY-+([\s\S]+)-+END EC PRIVATE KEY-+/m;
+var pemRfc5915RE = /-+BEGIN EC PRIVATE KEY-+([\s\S]+)-+END EC PRIVATE KEY-+/m;
 var pemPkcs8RE   = /-+BEGIN PRIVATE KEY-+([\s\S]*)-+END PRIVATE KEY-+/m;
 var pemSpkiRE    = /-+BEGIN PUBLIC KEY-+([\s\S]*)-+END PUBLIC KEY-+/m;
 
@@ -167,16 +168,16 @@ function parsePem(pem) {
   if (! util.isString(pem)) throw new TypeError("PEM must be a string");
 
   var match = null;
-  if (match = pem.match(pemOpenSSLRE)) {
-    var buffer = new Buffer(match[1].replace(/\s/mg, ''), 'base64');
-    return parseOpenSSL(buffer);
+  if (match = pem.match(pemRfc5915RE)) {
+    var buffer = new Buffer(match[1].replace(/[\s-]/mg, ''), 'base64');
+    return parseRfc5915(buffer);
 
   } else if (match = pem.match(pemPkcs8RE)) {
-    var buffer = new Buffer(match[1].replace(/\s/mg, ''), 'base64');
+    var buffer = new Buffer(match[1].replace(/[\s-]/mg, ''), 'base64');
     return parsePkcs8(buffer);
 
   } else if (match = pem.match(pemSpkiRE)) {
-    var buffer = new Buffer(match[1].replace(/\s/mg, ''), 'base64');
+    var buffer = new Buffer(match[1].replace(/[\s-]/mg, ''), 'base64');
     return parseSpki(buffer);
 
   } else {
@@ -190,31 +191,84 @@ function parsePem(pem) {
  * ========================================================================== */
 
 function ECDH(key) {
-  var curve, privateKey, publicKey, x, y;
+  var curve, d, x, y;
 
   if (util.isString(key)) {
     var k = parsePem(key);
     curve = k.c;
     x = k.x;
     y = k.y;
-    privateKey = k.d;
-    publicKey = Buffer.concat([new Buffer([0x04]), x, y]);
+    d = k.d;
+  } else if (util.isObject(key)) {
+    // Curves
+    if (util.isString(key.curve)) {
+      curve = key.curve;
+    } else if (util.isString(key.crv)) {
+      curve = curves[key.crv] || key.crv;
+    }
+
+    // Private key or "d"
+    if (util.isBuffer(key.privateKey)) {
+      d = key.privateKey;
+    } else if (util.isString(key.privateKey)) {
+      d = base64.decode(key.privateKey);
+    } else if (util.isBuffer(key.d)) {
+      d = key.d;
+    } else if (util.isString(key.d)) {
+      d = base64.decode(key.d);
+    }
+
+    // Public key, or x and y
+    if (util.isBuffer(key.publicKey)) {
+      var k = parsePublicKeyBuffer(curve, key.publicKey);
+      x = k.x;
+      y = k.y;
+
+    } else if (util.isString(key.publicKey)) {
+      var k = parsePublicKeyBuffer(curve, base64.decode(key.publicKey));
+      x = k.x;
+      y = k.y;
+
+    } else {
+      // Need to get x and y
+      if (util.isBuffer(key.x)) {
+        x = key.x;
+      } else if (util.isString(key.x)) {
+        x = base64.decode(key.x);
+      }
+
+      if (util.isBuffer(key.y)) {
+        y = key.y;
+      } else if (util.isString(key.y)) {
+        y = base64.decode(key.y);
+      }
+    }
+
   } else {
     throw new TypeError('Unrecognized format for EC key');
   }
 
+  // Validate curve, d, x and y
+  if (! curve) throw new TypeError("EC Key curve not specified");
+  if ((! x) || (! y)) throw new TypeError("Public EC Key point unavailable");
+
+  var length = lengths[curve];
+  if (! length) throw new TypeError("EC Key curve \"" + curve + "\" invalid");
+  if (x.length != length) throw new TypeError("Public EC Key point X of wrong length");
+  if (y.length != length) throw new TypeError("Public EC Key point Y of wrong length");
+  if (d && (y.length != length)) throw new TypeError("Private EC Key of wrong length");
+
+  // Define our properties
   Object.defineProperties(this, {
     'curve': {
       enumerable: true,
       configurable: false,
       value: curve
     },
-    'publicKey': {
+    'isPrivateECKey': {
       enumerable: true,
-      configurable: false, get:
-      function() {
-        return new Buffer(publicKey)
-      }
+      configurable: false,
+      value: (d != null)
     },
     'x': {
       enumerable: true,
@@ -232,13 +286,129 @@ function ECDH(key) {
     },
   });
 
-  if (privateKey) Object.defineProperty(this, 'privateKey', {
+  // The "d" (private key) is optional
+  if (d) Object.defineProperty(this, 'd', {
     enumerable: true,
     configurable: false,
     get: function() {
-      return new Buffer(privateKey)
+      return new Buffer(d)
     }
   });
+}
+
+/* ========================================================================== *
+ * CONVERSION                                                                 *
+ * ========================================================================== */
+
+ECDH.prototype.toPublicECKey = function() {
+  if (! this.isPrivateECKey) return this;
+  return new ECDH({
+    curve: this.curve,
+    x: this.x,
+    y: this.y
+  });
+}
+
+ECDH.prototype.toBuffer = function(format) {
+  if (this.isPrivateECKey) {
+    // Strip leading zeroes from private key
+    var d = this.d;
+    while (d[0] == 0) d = d.slice(1);
+
+    // Known formats: "pkcs8" (default), "pem", "openssl"
+    if (! format) format = "pkcs8";
+    if ((format == "pkcs8") || (format == "rfc5208")) {
+
+      // Encode in PKCS8
+      return ASN1ECPkcs8Key.encode({
+        version: 0,
+        algorithmIdentifier: {
+          privateKeyType: 'EC',
+          parameters: this.curve,
+        },
+        // Private key is RFC5915 minus curve
+        privateKey: ASN1ECRfc5915Key.encode({
+          version: 1,
+          privateKey: d,
+          publicKey: { data: Buffer.concat([new Buffer([0x04]), this.x, this.y]) }
+        }, 'der')
+      }, 'der');
+
+    } else if ((format == "openssl") || (format == "rfc5915")) {
+
+      // Simply encode in ASN.1
+      return ASN1ECRfc5915Key.encode({
+        version: 1,
+        privateKey: d,
+        parameters: this.curve,
+        publicKey: { data: Buffer.concat([new Buffer([0x04]), this.x, this.y]) }
+      }, 'der');
+
+    } else if ((format == "spki") || (format == "rfc5280")) {
+      return this.toPublicECKey().toBuffer("spki");
+
+    } else {
+      throw new TypeError("Unknown format for private key \"" + format + "\"");
+    }
+
+  } else {
+
+    if (! format) format = "spki";
+    if ((format == "spki") || (format == "rfc5280")) {
+      return ASN1ECSpkiKey.encode({
+        algorithmIdentifier: {
+          publicKeyType: 'EC',
+          parameters: this.curve
+        },
+        publicKey: { data: Buffer.concat([new Buffer([0x04]), this.x, this.y]) }
+      }, 'der');
+
+    } else {
+      throw new TypeError("Unknown format for public key \"" + format + "\"");
+    }
+  }
+}
+
+ECDH.prototype.toString = function(format) {
+  if (this.isPrivateECKey) {
+    if (! format) format = "pem";
+    if (format == "pem") { // pkcs8, wrapped
+      return '-----BEGIN PRIVATE KEY-----\n'
+           + this.toBuffer('pkcs8').toString('base64').match(/.{1,64}/g).join('\n')
+           + '\n-----END PRIVATE KEY-----\n';
+
+    } else if (format == "rfc5915") { // rfc5915, wrapped
+      return '-----BEGIN EC PRIVATE KEY-----\n'
+           + this.toBuffer('rfc5915').toString('base64').match(/.{1,64}/g).join('\n')
+           + '\n-----END EC PRIVATE KEY-----\n';
+
+    } else if ((format == "pkcs8") || (format == "rfc5208")) {
+      return this.toBuffer('pkcs8').toString('base64');
+
+    } else if ((format == "pkcs8-urlsafe") || (format == "rfc5208-urlsafe")) {
+      return base64.encode(this.toBuffer('pkcs8'));
+
+    } else if ((format == "spki") || (format == "rfc5280")) {
+      return this.toBuffer('spki').toString('base64');
+
+    } else if ((format == "spki-urlsafe") || (format == "rfc5280-urlsafe")) {
+      return base64.encode(this.toBuffer('spki'));
+
+    } else {
+      throw new TypeError("Unknown format for private key \"" + format + "\"");
+    }
+
+  } else {
+    if ((format == "spki") || (format == "rfc5280")) {
+      return this.toBuffer('spki').toString('base64');
+
+    } else if ((format == "spki-urlsafe") || (format == "rfc5280-urlsafe")) {
+      return base64.encode(this.toBuffer('spki'));
+
+    } else {
+      throw new TypeError("Unknown format for public key \"" + format + "\"");
+    }
+  }
 }
 
 ECDH.prototype.toJSON = function() {
@@ -249,8 +419,8 @@ ECDH.prototype.toJSON = function() {
     y: base64.encode(this.y),
   };
 
-  var privateKey = this.privateKey;
-  if (privateKey) jwk.d = base64.encode(privateKey);
+  var d = this.d;
+  if (d) jwk.d = base64.encode(d);
 
   return jwk;
 }
--- test/ecdh2.test.js
+++ test/ecdh2.test.js
@@ -2,8 +2,9 @@ var ECDH = require('../ecdh');
 var expect = require('chai').expect;
 var fs = require('fs');
 
-describe.only('ECDH 2', function() {
+describe.only('EC Key', function() {
 
+  var re = /-+BEGIN .* KEY-+([\s\S]+)-+END .* KEY-+/m;
   var names = [ 'prime256v1', 'secp384r1', 'secp521r1' ];
   var curves = {};
 
@@ -25,25 +26,96 @@ describe.only('ECDH 2', function() {
 
     describe('Curve ' + name, function() {
 
-      it('should parse an OpenSSL PEM private key', function() {
+      function testPublicKey(curve, key, name) {
+        // Curve name
+        expect(key.curve, "curve name").to.equal(name);
+
+        // JWK representation
+        expect(key.toJSON(), "jwk").to.eql(curve.pubJwk);
+
+        // Buffer: spki
+        expect(key.toBuffer('spki').toString('base64'))
+          .to.equal(curve.pub.match(re)[1].replace(/[\s-]/g, ''));
+
+        // Strings: spki normal and url safe
+        expect(key.toString('spki'))
+          .to.equal(curve.pub.match(re)[1].replace(/[\s-]/g, ''));
+        expect(key.toString('spki-urlsafe'))
+          .to.equal(curve.pub.match(re)[1].replace(/[\s-]/g, '')
+                                          .replace(/\+/g, '-')
+                                          .replace(/\//g, '_')
+                                          .replace(/=+$/g, ''));
+      }
+
+      function testPrivateKey(curve, key, name) {
+        // Curve name
+        expect(key.curve, "curve name").to.equal(name);
+
+        // JWK representation
+        expect(key.toJSON(), "jwk").to.eql(curve.privJwk);
+
+        // Buffers: pkcs8, openssl and spki (public)
+        expect(key.toBuffer('pkcs8').toString('base64'))
+          .to.equal(curve.pkcs8.match(re)[1].replace(/[\s-]/g, ''));
+        expect(key.toBuffer('openssl').toString('base64'))
+          .to.equal(curve.priv.match(re)[1].replace(/[\s-]/g, ''));
+        expect(key.toBuffer('spki').toString('base64'))
+          .to.equal(curve.pub.match(re)[1].replace(/[\s-]/g, ''));
+
+        // Strings: pem, rfc5951 (openssl)
+        expect(key.toString('pem')).to.equal(curve.pkcs8);
+        expect(key.toString('rfc5915')).to.equal(curve.priv);
+
+        // Strings: pkcs8 and spki (public) normal and url safe
+        expect(key.toString('pkcs8'))
+          .to.equal(curve.pkcs8.match(re)[1].replace(/[\s-]/g, ''));
+        expect(key.toString('spki'))
+          .to.equal(curve.pub.match(re)[1].replace(/[\s-]/g, ''));
+        expect(key.toString('pkcs8-urlsafe'))
+          .to.equal(curve.pkcs8.match(re)[1].replace(/[\s-]/g, '')
+                                            .replace(/\+/g, '-')
+                                            .replace(/\//g, '_')
+                                            .replace(/=+$/g, ''));
+        expect(key.toString('spki-urlsafe'))
+          .to.equal(curve.pub.match(re)[1].replace(/[\s-]/g, '')
+                                          .replace(/\+/g, '-')
+                                          .replace(/\//g, '_')
+                                          .replace(/=+$/g, ''));
+
+        // Conversion to public key and test
+        testPublicKey(curve, key.toPublicECKey(), name);
+      }
+
+      /* Run the tests per each source file */
+
+      it('should parse a OpenSSL PEM private key', function() {
         var curve = curves[name];
         var key = new ECDH(curve.priv);
-        expect(key.curve, "curve name").to.equal(name);
-        expect(key.toJSON(), "jwk").to.eql(curve.privJwk);
+        testPrivateKey(curve, key, name);
       });
 
       it('should parse a PKCS8 private key', function() {
         var curve = curves[name];
         var key = new ECDH(curve.pkcs8);
-        expect(key.curve, "curve name").to.equal(name);
-        expect(key.toJSON(), "jwk").to.eql(curve.privJwk);
+        testPrivateKey(curve, key, name);
+      });
+
+      it('should parse a JWK private key', function() {
+        var curve = curves[name];
+        var key = new ECDH(curve.privJwk);
+        testPrivateKey(curve, key, name);
       });
 
       it('should parse a SPKI public key', function() {
         var curve = curves[name];
         var key = new ECDH(curve.pub);
-        expect(key.curve, "curve name").to.equal(name);
-        expect(key.toJSON(), "jwk").to.eql(curve.pubJwk);
+        testPublicKey(curve, key, name);
+      });
+
+      it('should parse a JWK public key', function() {
+        var curve = curves[name];
+        var key = new ECDH(curve.pubJwk);
+        testPublicKey(curve, key, name);
       });
 
     });
