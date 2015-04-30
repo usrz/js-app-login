d85489acdddd8f9b304fce1b26c0bd23d84d28bb
Pier Fumagalli <pier@usrz.com>
2015-04-30 21:08:15 +0900

    Fixup.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express')();
-var login = require('./src/application.js');
+var server = require('./src/server.js');
 
-express.use('/login', login());
-server = express.listen(8080, '127.0.0.1', function(error) {
+var sessionManager = require('./src/sessionManager')('foobarbaz');
+express.locals.sessionManager = sessionManager;
+
+express.use('/login', server);
+listener = express.listen(8080, '127.0.0.1', function(error) {
+
   if (error) done(error);
-  var address = server.address();
+  var address = listener.address();
   console.log('Running at http://' + address.address + ':' + address.port + '/login');
 });
