7dcbbb8b4003745642eff28e28283d40533c8b7f
Pier Fumagalli <pier@usrz.com>
2015-05-03 17:47:17 +0900

    Adding tokens (replacement of SessionManager).

---
+++ src/tokens/index.js
@@ -0,0 +1,119 @@
+'use strict'
+
+const base64 = require('../util/base64');
+const crypto = require('crypto');
+const util = require('util');
+const ms = require('ms');
+
+const keys = new WeakMap();
+
+function getTimeBuffer(time) {
+  if (!(time >= new Date().getTime())) throw new Error('Wrong time "' + time + '"');
+  if (time > 8640000000000000) time = 8640000000000000;
+  var time_buffer = new Buffer(8);
+  time_buffer.writeUInt32BE(time / 0x100000000, 0);
+  time_buffer.writeUInt32BE(time % 0x100000000, 4);
+
+  return time_buffer;
+}
+
+function getAuthenticatedData(time_buffer, extra_auth_data) {
+
+  if ((!util.isBuffer(time_buffer)) || (time_buffer.length != 8)) {
+    throw new Error('Time must be a 8 bytes long buffer');
+  }
+
+  if (! extra_auth_data) return time_buffer;
+  if (util.isBuffer(extra_auth_data)) return Buffer.concat([time_buffer, extra_auth_data]);
+  throw new Error('Extra authenticated data must be a buffer or null');
+
+}
+
+function TokenManager(key) {
+  if (!(this instanceof TokenManager)) throw new Error('Can not create a TokenManager');
+
+  // Validate key
+  if (! util.isBuffer(key)) throw new TypeError('Encryption key must be buffer');
+  if (key.length < 1) throw new TypeError('Encryption key is empty');
+
+  // We use AES-256-GCM, need 256 bits for the key...
+  keys.set(this, crypto.createHash('sha256').update(key).digest());
+}
+
+TokenManager.prototype.create = function(timeout, secret, extra_auth_data) {
+
+  // Validate/normalize timeout
+  if (! timeout) throw new TypeError('Missing timeout');
+  if (util.isString(timeout)) timeout = ms(timeout) || timeout;
+  if (! util.isNumber(timeout)) throw new TypeError('Invalid timeout "' + timeout + '"');
+  if (timeout < 0) throw new TypeError('Timeout must be non negative');
+
+  // Validate secret and authenticated data
+  if (! util.isBuffer(secret)) throw new Error('Secret must be a buffer');
+
+  // Our authenticated data including time!
+  var expires_at = new Date().getTime() + timeout;
+  var time_buffer = getTimeBuffer(expires_at);
+  var auth_data = getAuthenticatedData(time_buffer, extra_auth_data);
+
+  // Good random initialization data
+  var init_vector = crypto.randomBytes(12);
+
+  // Encypher up the secret for this session
+  var key = keys.get(this);
+  var cipher = crypto.createCipheriv('aes-256-gcm', key, init_vector);
+  cipher.setAAD(auth_data);
+  cipher.write(secret);
+  cipher.end();
+
+  // Get the encrypted secret and auth tag
+  var encrypted_data = cipher.read();
+  var auth_tag = cipher.getAuthTag();
+
+  // Encode all in base64, big URL
+  return ( time_buffer.toString('base64') + '.'
+         + init_vector.toString('base64') + '.'
+         + encrypted_data.toString('base64') + '.'
+         + auth_tag.toString('base64')
+         ).replace(/\+/g, '-')
+          .replace(/\//g, '_')
+          .replace(/=/g,   '');
+}
+
+TokenManager.prototype.validate = function(token, extra_auth_data) {
+
+  // Validate and split token in 4 parts
+  if (! util.isString(token)) throw new Error('Token must be a string');
+  var components = token.replace(/\-/g, '+')
+                        .replace(/_/g,  '/')
+                        .split('.');
+  if (components.length !== 4) throw new Error('Invalid token');
+
+  // Parse out the buffers for decryption
+  var time_buffer = new Buffer(components[0], 'base64');
+  var init_vector = new Buffer(components[1], 'base64');
+  var encrypted_data = new Buffer(components[2], 'base64');
+  var auth_tag = new Buffer(components[3], 'base64');
+
+  // Check expiration time
+  var expires_at = time_buffer.readUInt32BE(0) * 0x100000000 +
+                   time_buffer.readUInt32BE(4);
+
+  if (new Date().getTime() > expires_at) return null;
+
+  // Our authenticated data including time buffer!
+  var auth_data = getAuthenticatedData(time_buffer, extra_auth_data);
+
+  // Attempt to decrypt
+  var key = keys.get(this);
+  var decipher = crypto.createDecipheriv('aes-256-gcm', key, init_vector);
+  decipher.setAAD(auth_data);
+  decipher.setAuthTag(auth_tag);
+  decipher.write(encrypted_data);
+  decipher.end();
+
+  // Return the decrypted secret
+  return decipher.read();
+}
+
+exports = module.exports = TokenManager;
---
+++ test/tokens.test.js
@@ -0,0 +1,127 @@
+'use strict';
+
+var expect = require('chai').expect;
+var crypto = require('crypto');
+
+var TokenManager = require('../src/tokens/index.js');
+
+describe.only('Tokens', function() {
+
+  var manager;
+  before(function() {
+    manager = new TokenManager(new Buffer('hello, world!'));
+  });
+
+  it('should encrypt and decrypt a secret', function() {
+    var secret = crypto.randomBytes(64);
+    var token = manager.create(1000, secret);
+
+    var check = manager.validate(token);
+    expect(check).to.be.instanceof(Buffer);
+    expect(check).to.eql(secret);
+  });
+
+  it('should throw an error for a wrong secret', function() {
+    var secret = crypto.randomBytes(64);
+    var token1 = manager.create(1000, secret);
+
+    var token2;
+    if (token1.substr(60,1) == 'x') {
+      token2 = token1.substr(0,60) + 'y' + token1.substr(61);
+    } else {
+      token2 = token1.substr(0,60) + 'x' + token1.substr(61);
+    }
+
+    expect(manager.validate(token1)).to.be.instanceof(Buffer);
+    expect(function() {
+        manager.validate(token2)
+      }).to.throw('Unsupported state or unable to authenticate data');
+  });
+
+  it('should throw an error for a wrong authentication data', function() {
+    var secret = crypto.randomBytes(64);
+    var token = manager.create(1000, secret);
+
+    expect(manager.validate(token)).to.be.instanceof(Buffer);
+    expect(function() {
+        manager.validate(token, new Buffer(1));
+      }).to.throw('Unsupported state or unable to authenticate data');
+  });
+
+  it('should throw an error for a wrong authentication data (part deux)', function() {
+    var secret = crypto.randomBytes(64);
+    var extra = new Buffer('hi!', 'utf8');
+    var token = manager.create(1000, secret, extra);
+
+    expect(manager.validate(token, extra)).to.be.instanceof(Buffer);
+    expect(function() {
+        manager.validate(token);
+      }).to.throw('Unsupported state or unable to authenticate data');
+  });
+
+  it('should never generate two equal tokens', function() {
+    var secret = new Buffer('This will expire in 300 millis', 'utf8');
+
+    // Overflow timeout, so we are always sure it will work
+    var token1 = manager.create(300, secret);
+    var token2 = manager.create(300, secret);
+
+    var components1 = token1.split('.');
+    var components2 = token2.split('.');
+    expect(components1.length).to.equal(4);
+    expect(components2.length).to.equal(4);
+
+    // Timeout matches...
+    expect(components1[0]).to.equal(components2[0]);
+
+    // IV, encrypted data, and authentication tag do not...
+    expect(components1[1]).to.not.equal(components2[1]);
+    expect(components1[2]).to.not.equal(components2[2]);
+    expect(components1[3]).to.not.equal(components2[3]);
+  });
+
+  it('should create a "forever" token', function() {
+    var secret = new Buffer('This will expire in year 275760')
+    var token = manager.create(Number.MAX_SAFE_INTEGER, secret);
+    expect(token).to.match(/^AB6yCMLcAAA\./);
+  })
+
+
+  it('should decrypt a well known token', function() {//AB6yCMLcAAA.NDDv0ImE2m43ZOUd.CaWkRLUKGxYPN9hmyKgV9sCJooXlwL04gw.Ump1WTrsgLVfFMYEIqE5SA
+    var secret = new Buffer('This will expire in year 275760')
+    expect(manager.validate('AB6yCMLcAAA.oO3Rgimm9CpKrLNy.Mwndc4UvDDf6uY4r-q9f5p65sesP_ABuLJmwzQW5eg.j0J9O0t6stLXFzo12Q9KNA'))
+      .to.eql(secret);
+  })
+
+  it('should return null decrypting an expired token', function() {
+    expect(manager.validate('AAABTRTVbX0.YSuEbasiKH3R4yeY.D9A3xJlCJom6_WPUJc9ZvvkuFS7JpYVeRDVS0aWECgjkLCYrNwlO9GECIaat0HHnsUPncHDpwINsV6FT4TEsOA.YkHMO-U7Q-9sqOn_VHKIbg'))
+      .to.be.null;
+  });
+
+  it('should honor timeouts', function(done) {
+    this.slow(200);
+    var secret = crypto.randomBytes(64);
+    var token = manager.create(40, secret);
+
+    setTimeout(function() {
+      try {
+        expect(manager.validate(token)).to.eql(secret);
+      } catch (error) {
+        done(error);
+      }
+    }, 20)
+
+    setTimeout(function() {
+      try {
+        expect(manager.validate(token)).to.be.null;
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }, 60);
+
+
+  })
+
+
+})
